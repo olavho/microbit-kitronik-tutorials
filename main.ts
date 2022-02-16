7808be7bd70c89bfe78590e16006f2317ee3161c
olavho <99804412+olavho@users.noreply.github.com>
2022-02-16 14:05:41 +0100

    kit02 - using a light sensor and analog inputs

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+let light2 = 0
+basic.forever(function () {
+    light2 = pins.analogReadPin(AnalogPin.P0)
+    if (light2 > 200) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # .
+            . # # # .
+            # # # . .
+            `)
+    }
+})
