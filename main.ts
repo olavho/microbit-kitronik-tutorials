edb8fde3fd5bade4acb4cfff31a5c86e127b0e47
olavho <99804412+olavho@users.noreply.github.com>
2022-02-16 16:23:26 +0100

    kit07 wind power

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
+let highest = 0
+let value = 0
+let running = 1;
 
+input.onButtonPressed(Button.A, function () {
+    running = 1;
+    basic.showNumber(highest)
+});
+
+input.onButtonPressed(Button.B, function () {
+    running = 0;
+    basic.showNumber(highest)
+});
+
+input.onButtonPressed(Button.AB, function () {
+    highest = 0
+    basic.showNumber(highest)
+});
+
+
+basic.forever(function () {
+    if (running === 1) {
+        value = pins.analogReadPin(AnalogPin.P0)
+        if (value > highest) {
+            highest = value;
+            basic.showIcon(IconNames.Happy);
+            basic.showNumber(value);
+        }
+    }
+})
