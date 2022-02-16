2783292948ca16d9bf2c1bdd83b961bc8c8d8bb8
olavho <99804412+olavho@users.noreply.github.com>
2022-02-16 16:01:19 +0100

    kit06 - setting the tone with a Piezo Buzzer

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonPressed(Button.A, function () {
+    music.playTone(440, music.beat(BeatFraction.Whole))
+})
+input.onButtonPressed(Button.AB, function () {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(262, music.beat(BeatFraction.Whole))
+})
