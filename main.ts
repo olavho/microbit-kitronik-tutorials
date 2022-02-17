475fb88ebcda404d391547023761d83bb2f5fc4d
olavho <99804412+olavho@users.noreply.github.com>
2022-02-17 09:09:11 +0100

    kit10 using an RGB LED

--- main.ts
+++ main.ts
@@ -1,1 +1,38 @@
+let blue = 0;
+let green = 0;
+let red = 0;
+let increaseOnly = true;
 
+input.onButtonPressed(Button.A, function() {
+    blue = 0;
+    green = 0;
+    red = 0;
+});
+
+input.onButtonPressed(Button.B, function() {
+    increaseOnly = !increaseOnly;
+});
+
+basic.forever(function () {
+	pins.analogWritePin(AnalogPin.P0, red);
+    pins.analogWritePin(AnalogPin.P1, green);
+    pins.analogWritePin(AnalogPin.P2, blue);
+
+    if (pins.digitalReadPin(DigitalPin.P8) == 1 && green < 1020) {
+        green = green + 10;
+    } else if (pins.digitalReadPin(DigitalPin.P8) == 1 && green == 1020 && !increaseOnly) {
+        green = 0;
+    }
+
+    if (pins.digitalReadPin(DigitalPin.P12) == 1 && red < 1020) {
+        red = red + 10;
+    } else if (pins.digitalReadPin(DigitalPin.P12) == 1 && red == 1020 && !increaseOnly) {
+        red = 0;
+    }
+
+    if (pins.digitalReadPin(DigitalPin.P16) == 1 && blue < 1020) {
+        blue = blue + 10;
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 1 && blue == 1020 && !increaseOnly) {
+        blue = 0;
+    }
+});
