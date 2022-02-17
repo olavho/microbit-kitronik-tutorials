a2e1af386ad5fc6e6e0c396cb8f4592df8d2b4ba
olavho <99804412+olavho@users.noreply.github.com>
2022-02-17 12:02:48 +0100

    zip09 - spin an LED based on a sensor reading

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-
-let ring = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB);
-ring.setBrightness(50);
-ring.showRainbow(1, 360);
-
+let item = 0
+let ring = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+ring.setBrightness(50)
+ring.setPixelColor(6, NeoPixelColors.Blue)
+ring.show()
 basic.forever(function () {
-    ring.rotate(1);
-    ring.show();
+    item = input.acceleration(Dimension.X) / 200
+    ring.rotate(item)
+    ring.show()
     pause(100);
-});
+})
