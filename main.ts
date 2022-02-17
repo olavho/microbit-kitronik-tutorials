05e6e406444304a1175d29bbe304f82e1281f097
olavho <99804412+olavho@users.noreply.github.com>
2022-02-17 11:22:08 +0100

    zip05 control two zip strips independently from different pins.

--- main.ts
+++ main.ts
@@ -1,21 +1,28 @@
 input.onButtonPressed(Button.A, function () {
-    strip.clear()
     strip.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(2000)
+    strip.clear()
+    strip.show()
 })
 input.onButtonPressed(Button.AB, function () {
     colorIdx = 0
     position = 0
-    pauseInterval = 1600
+    pauseInterval = 1000
     strip.setPixelColor(0, neopixel.colors(colors[colorIdx]))
 })
 input.onButtonPressed(Button.B, function () {
-    strip.showRainbow(1, 350)
+    strip2.showRainbow(1, 350)
+    basic.pause(2000)
+    strip2.clear()
+    strip2.show()
 })
 let position = 0
+let strip2: neopixel.Strip = null
 let strip: neopixel.Strip = null
 let pauseInterval = 0
 let colorIdx = 0
-let colors : Array<NeoPixelColors>;
+pauseInterval = 1000
+let colors: Array<NeoPixelColors>;
 colors = [
 NeoPixelColors.Red,
 NeoPixelColors.Orange,
@@ -27,9 +34,5 @@ NeoPixelColors.Violet,
 NeoPixelColors.Purple,
 NeoPixelColors.White
 ]
-pauseInterval = 1000
 strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-strip.showColor(neopixel.colors(NeoPixelColors.Red))
-basic.pause(pauseInterval)
-strip.clear()
-strip.show()
+strip2 = neopixel.create(DigitalPin.P1, 5, NeoPixelMode.RGB)
