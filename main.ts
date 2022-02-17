6ee557581bd29dc3c0deb50ddbadd9d89c85e72b
olavho <99804412+olavho@users.noreply.github.com>
2022-02-17 10:53:26 +0100

    zip04 controlling multiple LEDs

--- main.ts
+++ main.ts
@@ -1,42 +1,35 @@
-let colors : Array<NeoPixelColors>;
-colors = [NeoPixelColors.Red, NeoPixelColors.Orange, NeoPixelColors.Yellow, NeoPixelColors.Green, NeoPixelColors.Blue, NeoPixelColors.Indigo, NeoPixelColors.Violet, NeoPixelColors.Purple, NeoPixelColors.White];
-let colorIdx = 0;
-
-let pauseInterval = 1600;
-
-let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-let position = 0
-strip.show()
-
-basic.forever(function () {
-    if (position < 4) {
-        strip.rotate(1)
-        position = position + 1
-    } else {
-        strip.clear()
-        strip.setPixelColor(0, neopixel.colors(colors[colorIdx]));
-        position = 0
-    }
-    strip.show()
-    basic.pause(pauseInterval)
-});
-
 input.onButtonPressed(Button.A, function () {
-    pauseInterval = pauseInterval / 2;
-});
-
-input.onButtonPressed(Button.B, function() {
-    colorIdx++;
-    if (colorIdx == colors.length) {
-        colorIdx = 0;
-    }
-});
-
+    strip.clear()
+    strip.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Blue))
+})
 input.onButtonPressed(Button.AB, function () {
-    colorIdx = 0;
-    position = 0;
-    pauseInterval = 1600;
-
-    strip.setPixelColor(0, neopixel.colors(colors[colorIdx]));
-});
+    colorIdx = 0
+    position = 0
+    pauseInterval = 1600
+    strip.setPixelColor(0, neopixel.colors(colors[colorIdx]))
+})
+input.onButtonPressed(Button.B, function () {
+    strip.showRainbow(1, 350)
+})
+let position = 0
+let strip: neopixel.Strip = null
+let pauseInterval = 0
+let colorIdx = 0
+let colors : Array<NeoPixelColors>;
+colors = [
+NeoPixelColors.Red,
+NeoPixelColors.Orange,
+NeoPixelColors.Yellow,
+NeoPixelColors.Green,
+NeoPixelColors.Blue,
+NeoPixelColors.Indigo,
+NeoPixelColors.Violet,
+NeoPixelColors.Purple,
+NeoPixelColors.White
+]
+pauseInterval = 1000
+strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+strip.showColor(neopixel.colors(NeoPixelColors.Red))
+basic.pause(pauseInterval)
+strip.clear()
+strip.show()
